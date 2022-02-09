b082d2d08b45ce41ac2a4870ccbc4db03eec4574
Rea888 <m.rakhely.viktoria@gmail.com>
2022-02-09 22:26:17 +0100

    Almost ready

--- src/main/java/com/github/rviki/ActionWrapper.java
+++ src/main/java/com/github/rviki/ActionWrapper.java
@@ -14,25 +14,39 @@ public class ActionWrapper {
         this.scannerWrapper = scannerWrapper;
     }
 
-    public void receiveEvent(MessageCreateEvent event){
+    public void receiveEvent(MessageCreateEvent event) {
         String message = event.getMessageContent();
-        if (message.startsWith("!remindme ")) {
-            String fileContent;
-            try {
-                scannerWrapper.reader();
-                fileContent= scannerWrapper.getEventByDate(message.substring(message.indexOf(" ")+1,20));
-
-            } catch (FileNotFoundException e){
-                fileContent = "No one has wrote yet";
-
-            } catch (Exception e) {
-                fileContent = "Error occurred: " + e.getMessage();
-                e.printStackTrace();
-            }
-            message = message.substring(message.indexOf(" ") + 1);
-            String str = String.format("%s%n", message);
-            writerWrapper.write(str);
-            event.getChannel().sendMessage(fileContent);
+        if (message.startsWith("!remindme get ")) {
+            String result = getEvent(message);
+            event.getChannel().sendMessage(result);
+        } else if (message.startsWith("!remindme set")) {
+            setEvent(message);
+            event.getChannel().sendMessage("Message has been recorded.");
         }
+
+    }
+
+    String getEvent(String message) {
+        String fileContent;
+        try {
+            scannerWrapper.reader();
+            fileContent = scannerWrapper.getEventByDate(message.substring(message.indexOf(" get ") + 5, 24));
+
+        } catch (FileNotFoundException e) {
+            fileContent = "No one has wrote yet";
+
+        } catch (Exception e) {
+            fileContent = "Error occurred: " + e.getMessage();
+            e.printStackTrace();
+        }
+        return fileContent;
+    }
+
+
+    void setEvent(String message) {
+        message = message.substring(message.indexOf(" set ") + 5);
+        String str = String.format("%s%n", message);
+        writerWrapper.write(str);
+
     }
 }
